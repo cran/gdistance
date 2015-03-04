611fa22c1386faca4b5a8587c2c33177115b5236
Jacob van Etten <jacobvanetten@yahoo.com>
2015-03-04 00:00:00 +0000

    version 1.1-7

--- inst/doc/gdistance1.R
+++ inst/doc/gdistance1.R
@@ -2,7 +2,13 @@
 ### Encoding: ISO8859-1
 
 ###################################################
-### code chunk number 1: figure0
+### code chunk number 1: gdistance1.Rnw:125-126
+###################################################
+options(width=60)
+
+
+###################################################
+### code chunk number 2: figure0
 ###################################################
 library("gdistance")
 rex <- raster(matrix(1,4,4))
@@ -41,13 +47,13 @@ plot(g16,layout=gridLayout, add=TRUE, edge.color="black", vertex.color="black", 
 
 
 ###################################################
-### code chunk number 2: gdistanc00
+### code chunk number 3: gdistanc00
 ###################################################
 options(prompt = "R> ", continue = "+  ", width = 70, useFancyQuotes = FALSE)
 
 
 ###################################################
-### code chunk number 3: gdistance-1
+### code chunk number 4: gdistance-1
 ###################################################
 library("gdistance")
 r <- raster(ncol=3,nrow=3)
@@ -56,34 +62,34 @@ r
 
 
 ###################################################
-### code chunk number 4: figure1
+### code chunk number 5: figure1
 ###################################################
 plot(r, main="r")
 text(r)
 
 
 ###################################################
-### code chunk number 5: gdistance-2
+### code chunk number 6: gdistance-2
 ###################################################
 plot(r, main="r")
 text(r)
 
 
 ###################################################
-### code chunk number 6: gdistance-3
+### code chunk number 7: gdistance-3
 ###################################################
 r[] <- 1
 tr1 <- transition(r, transitionFunction=mean, directions=8)
 
 
 ###################################################
-### code chunk number 7: gdistance-4
+### code chunk number 8: gdistance-4
 ###################################################
 tr1
 
 
 ###################################################
-### code chunk number 8: gdistance-5
+### code chunk number 9: gdistance-5
 ###################################################
 r[] <- runif(9)
 ncf <- function(x) max(x) - x[1] + x[2]
@@ -92,7 +98,7 @@ tr2
 
 
 ###################################################
-### code chunk number 9: gdistance-6
+### code chunk number 10: gdistance-6
 ###################################################
 tr3 <- tr1*tr2
 tr3 <- tr1+tr2
@@ -101,40 +107,40 @@ tr3 <- sqrt(tr1)
 
 
 ###################################################
-### code chunk number 10: gdistance-7
+### code chunk number 11: gdistance-7
 ###################################################
-tr3[cbind(1:9,1:9)] <- tr2[cbind(1:9,1:9)]
-tr3[1:9,1:9] <- tr2[1:9,1:9]
-tr3[1:5,1:5]
+tr3[cbind(1:9, 1:9)] <- tr2[cbind(1:9, 1:9)]
+tr3[1:9, 1:9] <- tr2[1:9, 1:9]
+tr3[1:5, 1:5]
 
 
 ###################################################
-### code chunk number 11: gdistance-8 (eval = FALSE)
+### code chunk number 12: gdistance-8 (eval = FALSE)
 ###################################################
 ## image(transitionMatrix(tr1))
 
 
 ###################################################
-### code chunk number 12: figure2
+### code chunk number 13: figure2
 ###################################################
 print(image(transitionMatrix(tr1)))
 
 
 ###################################################
-### code chunk number 13: figure3
+### code chunk number 14: figure3
 ###################################################
 plot(raster(tr3), main="raster(tr3)")
 
 
 ###################################################
-### code chunk number 14: gdistance-9
+### code chunk number 15: gdistance-9
 ###################################################
 tr1C <- geoCorrection(tr1, type="c", multpl=FALSE)
 tr2C <- geoCorrection(tr2, type="c", multpl=FALSE)
 
 
 ###################################################
-### code chunk number 15: gdistance-10
+### code chunk number 16: gdistance-10
 ###################################################
 r3 <- raster(ncol=18, nrow=9)
 r3 <- setValues(r3, runif(18*9)+5)
@@ -144,20 +150,20 @@ tr3R <- geoCorrection(tr3, type="r", multpl=FALSE, scl=TRUE)
 
 
 ###################################################
-### code chunk number 16: gdistance-11
+### code chunk number 17: gdistance-11
 ###################################################
 CorrMatrix <- geoCorrection(tr3, type="r", multpl=TRUE, scl=TRUE)
 tr3R <- tr3 * CorrMatrix
 
 
 ###################################################
-### code chunk number 17: gdistance-12
+### code chunk number 18: gdistance-12
 ###################################################
 sP <- cbind(c(-100, 100, -100), c(-50, 50, 50))
 
 
 ###################################################
-### code chunk number 18: gdistance-13
+### code chunk number 19: gdistance-13
 ###################################################
 costDistance(tr3C, sP)
 commuteDistance(tr3R, sP)
@@ -165,61 +171,61 @@ rSPDistance(tr3R, sP, sP, theta=1e-12, totalNet="total")
 
 
 ###################################################
-### code chunk number 19: gdistance-14
+### code chunk number 20: gdistance-14
 ###################################################
 origin <- SpatialPoints(cbind(0, 0))
 rSPraster <- passage(tr3C, origin, sP[3,], theta=3)
 
 
 ###################################################
-### code chunk number 20: figure4
+### code chunk number 21: figure4
 ###################################################
 plot(rSPraster, main="rSPraster")
 
 
 ###################################################
-### code chunk number 21: gdistance-15
+### code chunk number 22: gdistance-15
 ###################################################
 r1 <- passage(tr3C, origin, sP[1,], theta=1)
 r2 <- passage(tr3C, origin, sP[3,], theta=1)
-rJoint <- min(r1, r2) #Figure 6
-rDiv <- max(max(r1, r2) * (1 - min(r1, r2)) - min(r1, r2), 0) #Figure 7
+rJoint <- min(r1, r2)
+rDiv <- max(max(r1, r2) * (1 - min(r1, r2)) - min(r1, r2), 0) 
 
 
 ###################################################
-### code chunk number 22: figure5
+### code chunk number 23: figure5
 ###################################################
 plot(rJoint, main="rJoint")
 
 
 ###################################################
-### code chunk number 23: figure6
+### code chunk number 24: figure6
 ###################################################
 plot(rDiv, main="rDiv")
 
 
 ###################################################
-### code chunk number 24: gdistance-17
+### code chunk number 25: gdistance-17
 ###################################################
 pathInc(tr3C, origin, sP)
 
 
 ###################################################
-### code chunk number 25: figure7
+### code chunk number 26: figure7
 ###################################################
 plot(function(x)6 * exp(-3.5 * abs(x + 0.05)), -1, 1, xlab="slope", ylab="speed (m/s)")
 lines(cbind(c(0,0),c(0,6)), lty="longdash")
 
 
 ###################################################
-### code chunk number 26: gdistance-18
+### code chunk number 27: gdistance-18
 ###################################################
 r <- raster(system.file("external/maungawhau.grd", 
   package="gdistance"))
 
 
 ###################################################
-### code chunk number 27: gdistance-19
+### code chunk number 28: gdistance-19
 ###################################################
 altDiff <- function(x){x[2] - x[1]}
 hd <- transition(r,altDiff,8,symm=FALSE)
@@ -227,7 +233,7 @@ slope <- geoCorrection(hd, scl=FALSE)
 
 
 ###################################################
-### code chunk number 28: gdistance-20
+### code chunk number 29: gdistance-20
 ###################################################
 adj <- adjacent(r, cells=1:ncell(r), pairs=TRUE, directions=8)
 speed <- slope
@@ -235,42 +241,42 @@ speed[adj] <- 6 * exp(-3.5 * abs(slope[adj] + 0.05))
 
 
 ###################################################
-### code chunk number 29: gdistance-21
+### code chunk number 30: gdistance-21
 ###################################################
 x <- geoCorrection(speed, scl=FALSE) 
 
 
 ###################################################
-### code chunk number 30: gdistance-22
+### code chunk number 31: gdistance-22
 ###################################################
-A <- c(2667670,6479000)
-B <- c(2667800,6479400)
+A <- c(2667670, 6479000)
+B <- c(2667800, 6479400)
 AtoB <- shortestPath(x, A, B, output="SpatialLines")
 BtoA <- shortestPath(x, B, A, output="SpatialLines")
 
 
 ###################################################
-### code chunk number 31: fig8plot
+### code chunk number 32: fig8plot
 ###################################################
 plot(r, main="")
 lines(AtoB, col="red", lwd=2)
 lines(BtoA, col="blue")
-text(A[1]-10,A[2]-10,"A")
-text(B[1]+10,B[2]+10,"B")
+text(A[1] - 10, A[2] - 10, "A")
+text(B[1] + 10, B[2] + 10, "B")
 
 
 ###################################################
-### code chunk number 32: fig8
+### code chunk number 33: fig8
 ###################################################
 plot(r, main="")
 lines(AtoB, col="red", lwd=2)
 lines(BtoA, col="blue")
-text(A[1]-10,A[2]-10,"A")
-text(B[1]+10,B[2]+10,"B")
+text(A[1] - 10, A[2] - 10, "A")
+text(B[1] + 10, B[2] + 10, "B")
 
 
 ###################################################
-### code chunk number 33: gdistance-24
+### code chunk number 34: gdistance-24
 ###################################################
 Europe <- raster(system.file("external/Europe.grd", 
   package="gdistance"))
@@ -281,14 +287,14 @@ pC <- as.matrix(popCoord[c("x","y")])
 
 
 ###################################################
-### code chunk number 34: gdistance1.Rnw:726-728
+### code chunk number 35: gdistance1.Rnw:730-732
 ###################################################
 plot(Europe, main="")
 text(pC[,1],pC[,2],unlist(popCoord["Population"]),cex=.7)
 
 
 ###################################################
-### code chunk number 35: gdistance-25
+### code chunk number 36: gdistance-25
 ###################################################
 geoDist <- pointDistance(pC, longlat=TRUE)
 geoDist <- as.dist(geoDist)
@@ -304,7 +310,7 @@ cor(genDist,resDist)
 
 
 ###################################################
-### code chunk number 36: gdistance-26
+### code chunk number 37: gdistance-26
 ###################################################
 origin <- unlist(popCoord[22,c("x","y")])
 pI <- pathInc(trC, origin=origin, from=pC, 
